eab388eddea065cf23fbdf1f991d1ae71c93ec0a
Mathieu <mathieu2991@gmail.com>
2023-08-29 22:38:52 +0200

    fix bdd

--- SQL/mysql.sql
+++ SQL/mysql.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.3
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: mysql
--- Generation Time: Aug 25, 2023 at 11:05 AM
--- Server version: 10.6.12-MariaDB-1:10.6.12+maria~ubu2004-log
--- PHP Version: 8.1.14
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mar. 29 août 2023 à 20:37
+-- Version du serveur : 8.0.32
+-- Version de PHP : 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,232 +18,178 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `PokemonZoo`
+-- Base de données : `pokemonzoo`
 --
-CREATE DATABASE IF NOT EXISTS `PokemonZoo` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `PokemonZoo`;
+CREATE DATABASE IF NOT EXISTS `pokemonzoo` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `pokemonzoo`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `fences`
+-- Structure de la table `fences`
 --
 
-CREATE TABLE `fences` (
-  `id` int(11) NOT NULL,
-  `nameFence` varchar(30) NOT NULL,
-  `cleanliness` varchar(10) NOT NULL,
-  `type` varchar(10) NOT NULL,
-  `background` varchar(50) NOT NULL,
-  `population` int(11) NOT NULL,
-  `zoo_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `fences`;
+CREATE TABLE IF NOT EXISTS `fences` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nameFence` varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
+  `cleanliness` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
+  `type` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
+  `background` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `population` int NOT NULL,
+  `zoo_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `zoo_id` (`zoo_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `fences`
+-- Déchargement des données de la table `fences`
 --
 
 INSERT INTO `fences` (`id`, `nameFence`, `cleanliness`, `type`, `background`, `population`, `zoo_id`) VALUES
-(1, 'Reserve', 'Propre', 'Reserve', 'images/reserve.jpg', 5, 1),
-(5, 'Enclos des carapuces', 'Propre', 'Aquarium', 'images/aquarium.jpg', 4, 1),
-(6, 'Enclos des salamèches', 'Propre', 'Normal', 'images/normal.jpg', 2, 1),
-(19, 'Forêt de BourgPalette', 'Propre', 'Forest', 'images/forest.jpg', 0, 1);
+(33, 'Reserve', 'Correct', 'Reserve', 'images/reserve.jpg', 0, 6),
+(34, 'Enclos de la poiscaille', 'Propre', 'Aquarium', 'images/aquarium.jpg', 2, 6),
+(35, 'Forêt', 'Propre', 'Forest', 'images/forest.jpg', 1, 6);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pokemons`
+-- Structure de la table `pokemons`
 --
 
-CREATE TABLE `pokemons` (
-  `id` int(11) NOT NULL,
-  `age` int(11) NOT NULL,
-  `sex` varchar(10) NOT NULL,
-  `weight` int(11) NOT NULL,
-  `height` int(11) NOT NULL,
-  `health` int(11) NOT NULL DEFAULT 100,
+DROP TABLE IF EXISTS `pokemons`;
+CREATE TABLE IF NOT EXISTS `pokemons` (
+  `idPokemon` int NOT NULL AUTO_INCREMENT,
+  `age` int NOT NULL,
+  `sex` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
+  `weight` int NOT NULL,
+  `height` int NOT NULL,
+  `health` int NOT NULL DEFAULT '100',
   `hungry` tinyint(1) NOT NULL,
   `sleepy` tinyint(1) NOT NULL,
+  `sleeping` tinyint(1) NOT NULL,
   `sick` tinyint(1) NOT NULL,
-  `species_id` int(11) NOT NULL,
-  `fence_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `species_id` int NOT NULL,
+  `fence_id` int DEFAULT NULL,
+  PRIMARY KEY (`idPokemon`),
+  KEY `fence_id` (`fence_id`),
+  KEY `species_id` (`species_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=184 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `pokemons`
+-- Déchargement des données de la table `pokemons`
 --
 
-INSERT INTO `pokemons` (`id`, `age`, `sex`, `weight`, `height`, `health`, `hungry`, `sleepy`, `sick`, `species_id`, `fence_id`) VALUES
-(9, 4, 'male', 8, 50, 100, 0, 0, 0, 5, 5),
-(10, 2, 'female', 4, 25, 100, 0, 0, 0, 5, 5),
-(12, 0, 'female', 3, 10, 100, 0, 0, 0, 5, 5),
-(14, 0, 'female', 2, 12, 100, 0, 0, 0, 6, 6),
-(21, 0, 'male', 7, 15, 100, 0, 0, 0, 7, 5),
-(23, 0, 'female', 3, 10, 100, 0, 0, 0, 5, 1),
-(24, 0, 'female', 2, 12, 100, 0, 0, 0, 6, 6),
-(26, 0, 'female', 7, 15, 100, 1, 0, 0, 7, 1),
-(29, 0, 'female', 3, 10, 100, 0, 0, 0, 5, 1),
-(30, 0, 'female', 7, 15, 100, 1, 1, 0, 7, 1),
-(31, 0, 'male', 3, 10, 100, 1, 1, 0, 5, 1);
+INSERT INTO `pokemons` (`idPokemon`, `age`, `sex`, `weight`, `height`, `health`, `hungry`, `sleepy`, `sleeping`, `sick`, `species_id`, `fence_id`) VALUES
+(180, 6, 'female', 10, 50, 100, 0, 0, 0, 0, 8, 34),
+(182, 6, 'male', 8, 70, 100, 0, 0, 1, 0, 14, 35),
+(183, 6, 'female', 20, 99, 100, 0, 0, 0, 0, 7, 34);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `species`
+-- Structure de la table `species`
 --
 
-CREATE TABLE `species` (
-  `id` int(11) NOT NULL,
-  `name` varchar(20) NOT NULL,
-  `diet` varchar(30) NOT NULL,
-  `Type1` varchar(30) NOT NULL,
-  `Type2` varchar(30) DEFAULT NULL,
-  `avatar` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `species`;
+CREATE TABLE IF NOT EXISTS `species` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `diet` varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
+  `Type1` varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
+  `Type2` varchar(30) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `avatar` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `species`
+-- Déchargement des données de la table `species`
 --
 
 INSERT INTO `species` (`id`, `name`, `diet`, `Type1`, `Type2`, `avatar`) VALUES
 (5, 'Carapuce', 'Végétarien', 'Eau', NULL, 'images/carapuce.gif'),
 (6, 'Salamèche', 'Carnivore', 'Feu', NULL, 'images/salamèche.gif'),
-(7, 'Psykokwak', 'Végétarien', 'Eau', NULL, 'images/psykokwak.gif');
+(7, 'Psykokwak', 'Végétarien', 'Eau', NULL, 'images/psykokwak.gif'),
+(8, 'Carabaffe', 'Végétarien', 'Eau', NULL, 'images/carabaffe.gif'),
+(9, 'Tortank', 'Omnivore', 'Eau', NULL, 'images/tortank.gif'),
+(10, 'Reptincel', 'Carnivore', 'Feu', NULL, 'images/reptincel.gif'),
+(11, 'Dracaufeu', 'Carnivore', 'Feu', 'Vol', 'images/dracaufeu.gif'),
+(12, 'Akwakwak', 'Carnivore', 'Eau', NULL, 'images/akwakwak.gif'),
+(13, 'Bulbizarre', 'Végétarien', 'Plante', 'Poison', 'images/bulbizarre.gif'),
+(14, 'Herbizarre', 'Végétarien', 'Plante', 'Poison', 'images/herbizarre.gif'),
+(15, 'Florizarre', 'Végétarien', 'Plante', 'Poison', 'images/florizarre.gif'),
+(16, 'Chenipan', 'Végétarien', 'Insecte', NULL, 'images/chenipan.gif'),
+(17, 'Chrysacier', 'Végétarien', 'Insecte', NULL, 'images/chrysacier.gif'),
+(18, 'Papilusion', 'Omnivore', 'Insecte', 'Vol', 'images/papilusion.gif');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `staff`
+-- Structure de la table `staff`
 --
 
-CREATE TABLE `staff` (
-  `id` int(11) NOT NULL,
-  `nameEmployee` varchar(40) NOT NULL,
-  `age` int(3) NOT NULL,
-  `sex` varchar(15) NOT NULL,
-  `zoo_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `staff`;
+CREATE TABLE IF NOT EXISTS `staff` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nameEmployee` varchar(40) COLLATE utf8mb4_general_ci NOT NULL,
+  `age` int NOT NULL,
+  `sex` varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
+  `zoo_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `zoo_id` (`zoo_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `staff`
+-- Déchargement des données de la table `staff`
 --
 
 INSERT INTO `staff` (`id`, `nameEmployee`, `age`, `sex`, `zoo_id`) VALUES
-(2, 'Sasha', 15, 'male', 1);
+(6, 'Robert', 20, 'male', 6);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `zoo`
+-- Structure de la table `zoo`
 --
 
-CREATE TABLE `zoo` (
-  `id` int(11) NOT NULL,
-  `nameZoo` varchar(50) NOT NULL,
-  `numberMaxFences` int(11) NOT NULL,
-  `pokedollars` int(11) NOT NULL,
-  `time` int(11) NOT NULL,
-  `popularity` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `zoo`;
+CREATE TABLE IF NOT EXISTS `zoo` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nameZoo` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(500) COLLATE utf8mb4_general_ci NOT NULL,
+  `numberMaxFences` int NOT NULL,
+  `pokedollars` int NOT NULL,
+  `time` int NOT NULL,
+  `popularity` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `zoo`
+-- Déchargement des données de la table `zoo`
 --
 
-INSERT INTO `zoo` (`id`, `nameZoo`, `numberMaxFences`, `pokedollars`, `time`, `popularity`) VALUES
-(1, 'BourgPalette Zoo', 4, 100, 0, 0);
+INSERT INTO `zoo` (`id`, `nameZoo`, `password`, `numberMaxFences`, `pokedollars`, `time`, `popularity`) VALUES
+(6, 'Zoo de Roanne', '$2y$10$WyPP3Ik4vNh0qwOVeYmkmeYM7ugHV94MW4.rivSo4nqd766.Yg9S.', 4, 1, 6, 75);
 
 --
--- Indexes for dumped tables
---
-
---
--- Indexes for table `fences`
---
-ALTER TABLE `fences`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `zoo_id` (`zoo_id`);
-
---
--- Indexes for table `pokemons`
---
-ALTER TABLE `pokemons`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fence_id` (`fence_id`),
-  ADD KEY `species_id` (`species_id`);
-
---
--- Indexes for table `species`
---
-ALTER TABLE `species`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `staff`
---
-ALTER TABLE `staff`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `zoo_id` (`zoo_id`);
-
---
--- Indexes for table `zoo`
---
-ALTER TABLE `zoo`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
+-- Contraintes pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `fences`
---
-ALTER TABLE `fences`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
-
---
--- AUTO_INCREMENT for table `pokemons`
---
-ALTER TABLE `pokemons`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
-
---
--- AUTO_INCREMENT for table `species`
---
-ALTER TABLE `species`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `staff`
---
-ALTER TABLE `staff`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `zoo`
---
-ALTER TABLE `zoo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `fences`
+-- Contraintes pour la table `fences`
 --
 ALTER TABLE `fences`
   ADD CONSTRAINT `fence_of_zoo` FOREIGN KEY (`zoo_id`) REFERENCES `zoo` (`id`) ON DELETE CASCADE;
 
 --
--- Constraints for table `pokemons`
+-- Contraintes pour la table `pokemons`
 --
 ALTER TABLE `pokemons`
-  ADD CONSTRAINT `fence_of_animal` FOREIGN KEY (`fence_id`) REFERENCES `fences` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fence_of_animal` FOREIGN KEY (`fence_id`) REFERENCES `fences` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `species_of_animal` FOREIGN KEY (`species_id`) REFERENCES `species` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `staff`
+-- Contraintes pour la table `staff`
 --
 ALTER TABLE `staff`
   ADD CONSTRAINT `zoo_of_staff` FOREIGN KEY (`zoo_id`) REFERENCES `zoo` (`id`) ON DELETE CASCADE;
